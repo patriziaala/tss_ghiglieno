13ccacdd0ee7d51acda367e984c187688ba50f02
tss <tss@debian-tss24>
2016-05-13 12:26:17 +0200

    versione 03

--- src/main/java/it/tss/hello/git/App.java
+++ src/main/java/it/tss/hello/git/App.java
@@ -16,7 +16,9 @@ public class App {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Ciao Git");
+       
         System.out.println("Ciao Git");
+        System.out.println("Ciao pinot");
     }
     
 }
